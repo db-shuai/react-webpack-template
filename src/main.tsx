08ea23f8b68f6500d086f8a157fbe620d8d36622
sss <sss>
2022-12-01 11:10:57 +0800

    andt 升级

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import { ConfigProvider } from "antd";
-import 'antd/dist/antd.css';
+// import "antd/dist/antd.css";
 import zhCN from "antd/es/locale/zh_CN";
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
-import './styles/main.less';
-import AppRouter from './routers';
+import "./styles/main.less";
+import AppRouter from "./routers";
 
-dayjs.locale('zh-cn');
-
+dayjs.locale("zh-cn");
 
 const App = () => (
   <ConfigProvider locale={zhCN}>
@@ -17,9 +16,9 @@ const App = () => (
   </ConfigProvider>
 );
 
-const root = ReactDom.createRoot(document.getElementById('app') as HTMLElement);
+const root = ReactDom.createRoot(document.getElementById("app") as HTMLElement);
 
-root.render(<App />)
+root.render(<App />);
 
 /* // 热更新
 if (module.hot) {
